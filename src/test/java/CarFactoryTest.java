3ed81d48111bac79dd0bcd3b1972205da641968b
PalmquistS <71629477+PalmquistS@users.noreply.github.com>
2021-11-08 16:20:04 +0100

    step 1.1

---
+++ src/test/java/CarFactoryTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class CarFactoryTest {
+
+    @Test
+    void test_create_car_success() {
+        CarFactory carFactory = new CarFactory();
+        Car car = carFactory.createNewCar();
+        assertNotNull(car);
+    }
+}

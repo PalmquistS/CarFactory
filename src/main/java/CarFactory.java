e61ec7419c1e9e73de710b8ce00b80b2d96593df
PalmquistS <71629477+PalmquistS@users.noreply.github.com>
2021-11-10 14:15:19 +0100

    Step 2.2 more models and Step 2.3 dynamic models

--- src/main/java/CarFactory.java
+++ src/main/java/CarFactory.java
@@ -1,14 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class CarFactory {
     private VehicleRegistrationNumberGenerator vehicleRegistrationNumberGenerator;
     private String brand;
+    private Map<String, Model> models = new HashMap<>();
+
 
     public CarFactory(VehicleRegistrationNumberGenerator vehicleRegistrationNumberGenerator, String brand) {
         this.vehicleRegistrationNumberGenerator = vehicleRegistrationNumberGenerator;
         this.brand = brand;
     }
 
-    public Car createNewCar(String model, String color, String engineType, Integer numberOfPassengers, Integer enginePower) {
+    /*public Car createNewCar(String model, String color, String engineType, Integer numberOfPassengers, Integer enginePower) {
         return new Car(getBrand(), vehicleRegistrationNumberGenerator.getNextRegNo(), model, color, engineType, numberOfPassengers, enginePower);
+    }*/
+    public Car createNewCar(String modelAsText, String color) {
+        Model model = models.get(modelAsText);
+        if (model == null) {
+            throw new RuntimeException("Unknown model " + model);
+        }
+        return new Car(getBrand(), vehicleRegistrationNumberGenerator.getNextRegNo(), model.getModel(), color, model.getEngineType(), model.getNumberOfPassenger(), model.getEnginePower());
     }
 
     public String getBrand() {
@@ -17,5 +29,44 @@ public class CarFactory {
 
     public void setBrand(String brand) {
         this.brand = brand;
+    }
+
+    public void addModel(String model, String engineType, int enginePower, int numberOfPassenger) {
+        models.put(model, new Model(model, engineType, enginePower, numberOfPassenger));
+    }
+
+    public static class Model {
+        String model;
+        String engineType;
+        int enginePower;
+        int numberOfPassenger;
+
+        public Model(String model, String engineType, int enginePower, int numberOfPassenger) {
+            this.model = model;
+            this.engineType = engineType;
+            this.enginePower = enginePower;
+            this.numberOfPassenger = numberOfPassenger;
+        }
+
+        public String getModel() {
+            return model;
+        }
+
+
+        public String getEngineType() {
+            return engineType;
+        }
+
+
+        public int getEnginePower() {
+            return enginePower;
+        }
+
+
+        public int getNumberOfPassenger() {
+            return numberOfPassenger;
+        }
+
+
     }
 }
--- src/test/java/CarFactoryTest.java
+++ src/test/java/CarFactoryTest.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.List;
 
@@ -12,7 +14,8 @@ public class CarFactoryTest {
     void test_create_car_success() {
         VehicleRegistrationNumberGenerator vehicleRegistrationNumberGenerator = new VehicleRegistrationNumberGenerator(List.of("ABC123"));
         CarFactory carFactory = new CarFactory(vehicleRegistrationNumberGenerator, "Saab");
-        Car car = carFactory.createNewCar("900", "red", "Bensin", 4, 90);
+        carFactory.addModel("900",  "Bensin", 90, 4);
+        Car car = carFactory.createNewCar("900", "red");
 
         assertNotNull(car);
         assertEquals("red", car.getColor());
@@ -25,11 +28,36 @@ public class CarFactoryTest {
     void test_create_car_model_success() {
         VehicleRegistrationNumberGenerator vehicleRegistrationNumberGenerator = new VehicleRegistrationNumberGenerator(List.of("ABC123"));
         CarFactory carFactory = new CarFactory(vehicleRegistrationNumberGenerator, "Saab");
-        Car car = carFactory.createNewCar("900", "red", "Bensin", 4, 90);
+        carFactory.addModel("900",  "Bensin", 90, 4);
+        Car car = carFactory.createNewCar("900", "red");
 
         assertEquals("900", car.getModel());
         assertEquals("Bensin", car.getEngineType());
         assertEquals(90, car.getEnginePower());
         assertEquals(4, car.getNumberOfPassengers());
     }
+
+
+    @ParameterizedTest
+    @CsvSource({
+            "900 Turbo, red, Bensin/Turbo, 4, 150",
+            "93, red, Bensin, 4, 110",
+            "93 aero, red, Bensin/Turbo,4, 190",
+            "9-7x, red, Diesel/Turbo, 6, 170"
+    })
+    void test_create_many_car_model_success(String model, String color, String engineType, int numberOfPassenger, int enginePower) {
+        VehicleRegistrationNumberGenerator vehicleRegistrationNumberGenerator = new VehicleRegistrationNumberGenerator(List.of("ABC123"));
+        CarFactory carFactory = new CarFactory(vehicleRegistrationNumberGenerator, "Saab");
+
+        carFactory.addModel(model,engineType,enginePower,numberOfPassenger);
+        Car car = carFactory.createNewCar(model, color);
+        assertNotNull(car);
+        assertEquals(model, car.getModel());
+        assertEquals(color, car.getColor());
+        assertEquals(engineType, car.getEngineType());
+        assertEquals(enginePower, car.getEnginePower());
+        assertEquals(numberOfPassenger, car.getNumberOfPassengers());
+    }
+
+
 }
